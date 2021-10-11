a47c31afd42866ea40821834ff285f5cab1a9480
chenfengshf <chenfengshf@hotmail.com>
2021-10-11 09:10:06 +0800

    更新proxy4push

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 : for push to github
-git config --global https.proxy http://127.0.0.1:10808
+git config --global https.proxy https://127.0.0.1:10809
 timeout /T 5 /NOBREAK
-git push
+git push --set-upstream origin main
 git config --global --unset https.proxy
 git config --global -l
